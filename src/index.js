8aa00ed3b5919f51c5440c2209373f5283dbda5d
Alejandro Hussein <alejandrohussein@Nelmas-MBP.attlocal.net>
2023-02-19 18:31:57 -0600

    Module02

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 import './less/index.less'
 
 // Your code goes here!
+// const headerNav = document.getElementsByTagName("nav");
+// const navLinks = document.querySelectorAll("nav-link");
+// const pTag = document.getElementsByTagName('p')
+const heading = document.querySelector('header')
+const bodyPage =document.querySelector('body')
+
+
+document.body.addEventListener('click', evt => {
+    evt.target.classList = 'mirror'
+})
+document.body.addEventListener('mouseover', evt => {
+    evt.target.style.fontWeight = 'bold'
+})
+
+document.body.addEventListener('dblclick', evt => {
+    evt.target.outerHTML = ''
+})
+document.body.addEventListener('copy', () => {
+    navigator.clipboard.readText()
+    .then( text => heading.textContent += text)
+})
+function keydown(event) {
+    if(event.key === '5'){
+        bodyPage.style.backgroundColor = 'red'
+    }
+}
+document.addEventListener("keydown", keydown);
+
+document.body.addEventListener("select", evt => {
+    evt.target.style.color = 'yellow'
+}) 
+
+document.body.addEventListener('scroll', evt => {
+    evt.target.style.fontSize = "6rem"
+})
+
+console.log("hello"); 
